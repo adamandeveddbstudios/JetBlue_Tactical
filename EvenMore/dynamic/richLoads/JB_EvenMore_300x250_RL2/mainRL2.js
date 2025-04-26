151a896c384e11148e244d43b86f8c8a8ac2cbf0
Mahesh Bhatt <141926871+adamandeveddbstudios@users.noreply.github.com>
2025-04-26 22:45:08 +0530

    Add files via upload

--- EvenMore/dynamic/richLoads/JB_EvenMore_300x250_RL2/mainRL2.js
+++ EvenMore/dynamic/richLoads/JB_EvenMore_300x250_RL2/mainRL2.js
@@ -123,6 +123,8 @@ function setupContent() {
     // TESTING
 
     //  Longest possible places names
+    origin_formatted = 'Xxxx'
+    destination_formatted = 'Yyyy'
 
     //  Longest possible places names
     // origin_formatted = 'Providenciales, Turks and Caicos (PLS)'
